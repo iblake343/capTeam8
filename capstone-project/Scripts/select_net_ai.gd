b5abfeb76690bef52aa3d8d17c57e1e3e5340386
big-jeff12 <jmercer2@harding.edu>
2025-02-05 18:25:45 -0600

    Fixed Background play

--- capstone-project/Scripts/select_net_ai.gd
+++ capstone-project/Scripts/select_net_ai.gd
@@ -14,3 +14,7 @@ func _on_back_btn_pressed() -> void:
 
 func _on_network_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/PlaceTiles.tscn")
+
+
+func _on_ai_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/PlaceTiles.tscn")
