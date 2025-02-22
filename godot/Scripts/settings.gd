06bc14f63d0e1f4e9820cddb3a446cc9dd16b092
big-jeff12 <jmercer2@harding.edu>
2025-02-21 21:18:28 -0600

    Changed Tile Board to have random tiles
    This is a basic version. The random does not work like I want it to.

--- godot/Scripts/settings.gd
+++ godot/Scripts/settings.gd
@@ -9,6 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
-	
-func _on_back_btn_pressed() -> void:
+
+
+func _on_quit_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
