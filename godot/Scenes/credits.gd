eb0a6144e8b6ea7f2c6af53cdb76d0dda2ea277b
big-jeff12 <jmercer2@harding.edu>
2025-02-25 14:03:09 -0600

    Added Credit and Tutorial Base Scenes

--- godot/Scenes/credits.gd
+++ godot/Scenes/credits.gd
@@ -12,12 +12,4 @@ func _process(delta: float) -> void:
 
 
 func _on_quit_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/menu.tscn")
-
-
-func _on_settings_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/credits.tscn")
-
-
-func _on_start_game_btn_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/tutorial.tscn")
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
