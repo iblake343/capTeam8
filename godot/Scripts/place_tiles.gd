debcf3b85020ea43ef4d05b7adb5eb28e9e43281
big-jeff12 <jmercer2@harding.edu>
2025-02-25 18:47:49 -0600

    Made shark tiles placeable
    Started on the tile placing. First I added a number that could be overlayed, then a logo.

--- godot/Scripts/place_tiles.gd
+++ godot/Scripts/place_tiles.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 @onready var tilemaplayer: TileMapLayer = $ChipLayer  # Ensure this exists in your scene
+@onready var tilemapcount: TileMapLayer = $ChipLayer2
 
 func _ready():
 	tilemaplayer.set_cell(Vector2i(0,0), 0, Vector2i(0,0))
@@ -10,4 +11,6 @@ func _input(event):
 		var mouse_pos = tilemaplayer.get_local_mouse_position()
 		var tile_pos = tilemaplayer.local_to_map(mouse_pos)
 		#Will need to be told what user to place
-		tilemaplayer.set_cell(tile_pos, 0, Vector2i(0, 0))  # Set permanent change
+		tilemaplayer.set_cell(tile_pos, 2, Vector2i(0, 0))  # Set permanent change
+		tilemapcount.set_cell(tile_pos, 1, Vector2i(0, 0))
+		
