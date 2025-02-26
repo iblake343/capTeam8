ea443c5aceaf19836be0e3d180194aa19641ec01
big-jeff12 <jmercer2@harding.edu>
2025-02-25 20:28:15 -0600

    Added New Tile Assets

--- godot/Scripts/place_tiles.gd
+++ godot/Scripts/place_tiles.gd
@@ -4,13 +4,13 @@ extends Node2D
 @onready var tilemapcount: TileMapLayer = $ChipLayer2
 
 func _ready():
-	tilemaplayer.set_cell(Vector2i(0,0), 0, Vector2i(0,0))
-	
+	#tilemaplayer.set_cell(Vector2i(0,0), 0, Vector2i(0,0))
+	pass
 func _input(event):
 	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var mouse_pos = tilemaplayer.get_local_mouse_position()
 		var tile_pos = tilemaplayer.local_to_map(mouse_pos)
 		#Will need to be told what user to place
-		tilemaplayer.set_cell(tile_pos, 2, Vector2i(0, 0))  # Set permanent change
-		tilemapcount.set_cell(tile_pos, 1, Vector2i(0, 0))
+		tilemaplayer.set_cell(tile_pos, 0, Vector2i(0, 0))  # Set permanent change
+		#tilemapcount.set_cell(tile_pos, 1, Vector2i(0, 0))
 		
