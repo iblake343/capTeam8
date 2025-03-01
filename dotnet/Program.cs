d76890750614c812034c2fba5fc98cee6e6f4961
Andrew Phillips <addadp1961@gmail.com>
2025-02-28 18:49:52 -0600

    prompt user for game setup

--- dotnet/Program.cs
+++ dotnet/Program.cs
@@ -1,7 +1,16 @@
 ﻿
 var board = new Board();
-var player1 = new TuiPlayer();
-var player2 = new RandPlayer();
+
+Console.WriteLine("Is player 1 (Red) a computer (c) (default) or human (h)?");
+string p1opt = Console.ReadLine();
+Console.WriteLine("Is player 2 (Blue) a computer (c) (default) or human (h)?");
+string p2opt = Console.ReadLine();
+
+Player player1 =
+    (p1opt == "h" || p1opt == "human") ? new TuiPlayer() : new RandPlayer();
+Player player2 =
+    (p2opt == "h" || p2opt == "human") ? new TuiPlayer() : new RandPlayer();
+
 var display = new TuiDisplay();
 var game = new Game(
     new Player[] {player1, player2},
