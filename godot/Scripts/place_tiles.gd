5e23cafdfdad2a227d1864a786688bcfcdb28f9b
big-jeff12 <jmercer2@harding.edu>
2025-03-05 12:39:31 -0600

    Created logic for placing tile numbers during game

--- godot/Scripts/place_tiles.gd
+++ godot/Scripts/place_tiles.gd
@@ -2,42 +2,58 @@ extends Node2D
 
 @onready var tilemaplayer: TileMapLayer = $ChipLayer  # Ensure this exists in your scene
 @onready var tilemapcount: TileMapLayer = $ChipLayer2
-@onready var PlaceTile = true
-@onready var PickTile = false
+@onready var placing_tile = true
+@onready var selecting_tile = false
 @onready var first_click_position = null
+@onready var selected_tile_pos = null
+@onready var tileNumber = 0
+@onready var tilePlayer = 0
+@onready var playing_faze = false
 
 func _ready():
 	pass
 	
 func _input(event):
-	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and PlaceTile:
+	if event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT:
 		var mouse_pos = tilemaplayer.get_local_mouse_position()
 		var tile_pos = tilemaplayer.local_to_map(mouse_pos)
-		#Will need to be told what user to place
-		tilemaplayer.set_cell(tile_pos, randi_range(0,1), Vector2i(0, 0))  # Set permanent change
-		PlaceTile = false
-		#need to be told when to stop placing tiles.
-		PickTile = true
-		#tilemapcount.set_cell(tile_pos, 1, Vector2i(0, 0))
-	elif event is InputEventMouseButton and event.pressed and event.button_index == MOUSE_BUTTON_LEFT and PickTile:
-		#set cell at tile location. Will have to set the number from the game board.
-		if first_click_position == null:
-			first_click_position = event.position
-			var mouse_pos = tilemaplayer.get_local_mouse_position()
-			var tile_pos = tilemaplayer.local_to_map(mouse_pos)
-		else:
-			var tileNumber = 0
-			var tilePlayer = 0
-			move_tokens(tileNumber, tilePlayer)
-			first_click_position = null
-			
+		
+		if placing_tile and tilePlayer == 0 and !playing_faze:
+			#game core will have to see if original tile placement is valid
+			tilemaplayer.set_cell(tile_pos, 0, Vector2i(0, 0)) 
+			tilePlayer = 1
+		elif placing_tile and tilePlayer == 1 and !playing_faze:
+			#game core will have to see if original tile placement is valid
+			tilemaplayer.set_cell(tile_pos, 1, Vector2i(0, 0)) 
+			placing_tile = false
+			selecting_tile = true
+			playing_faze = true
+		elif playing_faze:
+			#will have to tell game core what tile was selected and receive validation
+			playing_faze = false
+		elif selecting_tile:
+			#game core will need to check the next click to see if it is correct.
+			#also will need to know the max amount of tiles that are able to be moved
+			#will cap the "tileNumber" depending on starting click
+			move_tile(selected_tile_pos, tile_pos)
+			if selected_tile_pos == null:
+				selected_tile_pos = tile_pos 
+			else:
+				selected_tile_pos = null
 
-		
-func move_tokens(tileNumber, tilePlayer):
-		var mouse_pos = tilemaplayer.get_local_mouse_position()
-		var tile_pos = tilemaplayer.local_to_map(mouse_pos)
-		tilemaplayer.set_cell(tile_pos, tileNumber, Vector2i(0,0))
-		tilemapcount.set_cell(tile_pos, tilePlayer, Vector2i(0,0))
-		
-func declare_winner():
-	pass # TODO implement
+	elif event is InputEventKey and event.pressed and event.keycode == KEY_SPACE:
+		playing_faze = true
+		if tilePlayer == 1:
+			tilePlayer = 0
+		else:
+			tilePlayer = 1
+
+func move_tile(from_pos, to_pos):
+
+	tilemaplayer.set_cell(to_pos, tilePlayer, Vector2i(0, 0))
+	tilemapcount.set_cell(to_pos, tileNumber, Vector2i(0, 0))
+	#tileNumber will be edit to have a max of how many tiles there are in starting stack
+	if tileNumber == 0:
+		tileNumber += 1
+	else:
+		tileNumber = 0
