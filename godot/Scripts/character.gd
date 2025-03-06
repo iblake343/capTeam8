abc3f334b5f29b61eee1c31df0e3ecaf4c63d78f
big-jeff12 <jmercer2@harding.edu>
2025-03-06 14:17:55 -0600

    Deleted Unneeded Scenes and Created highlight when press

--- godot/Scripts/character.gd
+++ godot/Scripts/character.gd
@@ -12,12 +12,12 @@ func _process(delta: float) -> void:
 
 
 func _on_button_pressed() -> void:
-	get_tree().change_scene_to_file("res://TileMapLayerGameGrid.tscn")
+	get_tree().change_scene_to_file("res://Scenes/TileMapLayerGameGrid.tscn")
 
 
 func _on_button_2_pressed() -> void:
-	get_tree().change_scene_to_file("res://TileMapLayerGameGrid.tscn")
+	get_tree().change_scene_to_file("res://Scenes/TileMapLayerGameGrid.tscn")
 
 
 func _on_button_3_pressed() -> void:
-	get_tree().change_scene_to_file("res://TileMapLayerGameGrid.tscn")
+	get_tree().change_scene_to_file("res://Scenes/TileMapLayerGameGrid.tscn")
--- godot/Scripts/credits.gd
+++ godot/Scripts/credits.gd
@@ -1,4 +1,4 @@
-extends Node2D
+extends CanvasLayer
 
 
 # Called when the node enters the scene tree for the first time.
@@ -9,3 +9,7 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+
+func _on_quit_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
