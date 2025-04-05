0414f67a51fe21c6e605b16008c8d8bdc792e231
Ibrahim Blake <ibrahimblake1@gmail.com>
2025-04-04 21:37:04 -0500

    spread germs

--- godot/Scripts/MultiplayerChoice.cs
+++ godot/Scripts/MultiplayerChoice.cs
@@ -4,16 +4,16 @@ using System;
 public partial class MultiplayerChoice : CanvasLayer
 {
 
-    [Export]
-    private int port = 8888;
-    
-    [Export]
-    private string address = "127.0.0.1";
+	[Export]
+	private int port = 8888;
+	
+	[Export]
+	private string address = "127.0.0.1";
 
-    private ENetMultiplayerPeer peer;
+	private ENetMultiplayerPeer peer;
 	// Called when the node enters the scene tree for the first time.
 
-    Label statusLbl;
+	Label statusLbl;
 	public override void _Ready()
 	{
 		Multiplayer.PeerConnected += PeerConnected;
@@ -21,88 +21,88 @@ public partial class MultiplayerChoice : CanvasLayer
 		Multiplayer.ConnectedToServer += ConnectedToServer;
 		Multiplayer.ConnectionFailed += ConnectionFailed;
 
-        statusLbl = GetNode<Label>("Control/status");
-        statusLbl.Text = "Choose to host or join a game";
+		statusLbl = GetNode<Label>("Control/status");
+		statusLbl.Text = "Choose to host or join a game";
 
 	}
 
-    private void ConnectionFailed()
-    {
-        GD.Print("CONNECTION FAILED");
-    }
+	private void ConnectionFailed()
+	{
+		GD.Print("CONNECTION FAILED");
+	}
 
 
-    private void ConnectedToServer()
-    {
-        GD.Print("Connected to server.");
-    }
+	private void ConnectedToServer()
+	{
+		GD.Print("Connected to server.");
+	}
 
 
-    private void PeerDisconnected(long id)
-    {
-        GD.Print("Player disconnected: " + id.ToString());
-    }
+	private void PeerDisconnected(long id)
+	{
+		GD.Print("Player disconnected: " + id.ToString());
+	}
 
-    private void PeerConnected(long id)
-    {
-        GD.Print("Player Connected: " + id.ToString());
-        statusLbl.Text = "Player  Connected";
+	private void PeerConnected(long id)
+	{
+		GD.Print("Player Connected: " + id.ToString());
+		statusLbl.Text = "Player  Connected";
 
-    }
+	}
 
-    // Called every frame. 'delta' is the elapsed time since the previous frame.
-    public override void _Process(double delta)
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+	public override void _Process(double delta)
 	{
 	}
 	
-    private void _on_back_btn_pressed()
-    {
-        GetTree().ChangeSceneToFile("res://Scenes/select_net_ai.tscn"); //fix 
-    }
+	private void _on_back_btn_pressed()
+	{
+		GetTree().ChangeSceneToFile("res://Scenes/select_net_ai.tscn"); //fix 
+	}
 
 	public void _on_host_pressed() { 
 		peer = new ENetMultiplayerPeer();
-        var error = peer.CreateServer(port, 2);
+		var error = peer.CreateServer(port, 2);
 
-        if (error != Error.Ok) { 
-            GD.Print("error cannot host! : " + error.ToString());
-        }
+		if (error != Error.Ok) { 
+			GD.Print("error cannot host! : " + error.ToString());
+		}
 
-        peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 
-        Multiplayer.MultiplayerPeer = peer;
-        GD.Print("Waiting For Players");
-        statusLbl.Text = "Waiting  for  players";
+		Multiplayer.MultiplayerPeer = peer;
+		GD.Print("Waiting For Players");
+		statusLbl.Text = "Waiting  for  players";
 
 
 	}
 	
 	public void _on_join_pressed() { 
 		peer = new ENetMultiplayerPeer();
-        peer.CreateClient(address, port);
+		peer.CreateClient(address, port);
 
-        peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
+		peer.Host.Compress(ENetConnection.CompressionMode.RangeCoder);
 
-        Multiplayer.MultiplayerPeer = peer;
-        GD.Print("Joining Game");
-        statusLbl.Text = "Joining  Game";
+		Multiplayer.MultiplayerPeer = peer;
+		GD.Print("Joining Game");
+		statusLbl.Text = "Joining  Game";
 
 
 	}
 
-    public void _on_start_pressed() { 
-        Rpc("startGame");
-    }
+	public void _on_start_pressed() { 
+		Rpc("startGame");
+	}
 
-    [Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
-    private void startGame() { 
-        // take them through these scenes after lobby filled 
-        // coin flip
-        // p1 chooses character then p2 
+	[Rpc(MultiplayerApi.RpcMode.AnyPeer, CallLocal = true, TransferMode = MultiplayerPeer.TransferModeEnum.Reliable)]
+	private void startGame() { 
+		// take them through these scenes after lobby filled 
+		// coin flip
+		// p1 chooses character then p2 
 
-        var scene = ResourceLoader.Load<PackedScene>("res://Scenes/character.tscn").Instantiate<Node2D>(); 
-        GetTree().Root.AddChild(scene);
-        this.Hide();
-    }
+		var scene = ResourceLoader.Load<PackedScene>("res://Scenes/character.tscn").Instantiate<Node2D>(); 
+		GetTree().Root.AddChild(scene);
+		this.Hide();
+	}
 	
 }
