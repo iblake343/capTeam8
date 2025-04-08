e3f752a57cd580daed39ff747862e295d2bc8035
big-jeff12 <jmercer2@harding.edu>
2025-04-08 14:26:14 -0500

    cleaned up scenes and changed resolution
    Lots of updating with this. Changed resolution, while also fixing the scene structure

--- godot/Scripts/menu.gd
+++ godot/Scripts/menu.gd
@@ -1,16 +1,7 @@
 extends CanvasLayer
-
-@onready var pan = $Panel
-@onready var btn = $"Panel/StartGameSubMenu/Single Player"
-@onready var btn1 = $"Panel/StartGameSubMenu/Two Player"
-@onready var btn2 = $Panel/StartGameSubMenu/Online
-var popbox = true
-var other = false
 static var background_scene = null
 
 func _ready():
-	# Initially hide the panel
-	pan.visible = false
 
 	# Check if background_scene is already in the scene tree
 	if not is_instance_valid(background_scene):
@@ -31,59 +22,19 @@ func _process(delta: float) -> void:
 	var animation_player = background_scene.get_node_or_null("AnimationPlayer")
 	if animation_player and not animation_player.is_playing():
 		animation_player.play("Animation", 0, true)
-	if !other and !popbox:
-		pan.visible = false
-
-# Function for showing the buttons when "Start Game" is pressed
-func _on_start_game_btn_pressed() -> void:
-	pan.visible = true
-	other = true
-	popbox = true
-
-func _on_start_game_btn_mouse_entered() -> void:
-	pan.visible = true
-	other = true
-	popbox = true
-
-func _on_start_game_btn_mouse_exited() -> void:
-	other = false
-	
-func _on_panel_mouse_exited() -> void:
-	popbox = false
-
-
-func _on_panel_mouse_entered() -> void:
-	popbox = true
-
-
-func _on_single_player_mouse_entered() -> void:
-	popbox = true
-
-
-
-func _on_two_player_mouse_entered() -> void:
-	popbox = true
-
-
-func _on_online_mouse_entered() -> void:
-	popbox = true
-
 
 func _on_single_player_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/difficulty.tscn")
+	
 
+func _on_two_player_btn_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/characterPlayer1.tscn")
+
+func _on_online_pressed() -> void:
+	get_tree().change_scene_to_file("res://Scenes/character.tscn")
 
 func _on_settings_btn_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/settings.tscn")
 
-
 func _on_quit_btn_pressed() -> void:
 	get_tree().quit()
-
-
-func _on_two_player_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/choose_characterScene.tscn")
-
-
-func _on_online_pressed() -> void:
-	get_tree().change_scene_to_file("res://Scenes/character.tscn")
