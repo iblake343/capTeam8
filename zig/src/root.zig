4d073497ca1a4d0f7aa0a8106718bb5c8efb4da4
Andrew Phillips <addadp1961@gmail.com>
2025-04-13 19:42:43 -0500

    update core.dll

--- zig/src/root.zig
+++ zig/src/root.zig
@@ -1055,7 +1055,7 @@ export fn xGetFrame(board: *const Board, coords: *[4]int) callconv(.C) void {
 }
 
 export fn xParse(board: *Board, src: [*c]u8) callconv(.C) bool {
-    data.* = .parse(std.mem.span(src)) orelse return false;
+    board.* = Board.parse(std.mem.span(src)) catch return false;
     return true;
 }
 
